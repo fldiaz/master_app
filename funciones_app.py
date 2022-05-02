5c048d416754edab5718c95310aa47a82c15b835
florencia <fldiaz@itba.edu.ar>
2022-05-02 15:01:08 -0300

    nueva_hoja

--- funciones_app.py
+++ funciones_app.py
@@ -10,41 +10,21 @@ import plotly.express as px
 warnings.filterwarnings('ignore')
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.feature_extraction.text import TfidfVectorizer
-pd.options.display.max_rows = 999
-
 import s3fs
 import os
 import boto3
 import time
 
-
-
-def tf(listados, ngram_range=(1,2)):
-    vectorizer = TfidfVectorizer(ngram_range=ngram_range, stop_words="english").fit(listados)
-    vector = vectorizer.transform(listados).toarray()
-    return vector, vectorizer
-
-
-def extract_top_n_words_per_topic(tf_idf, count, n=20):
-    words = count.get_feature_names()
-    tf_idf_transposed = tf_idf.T
-    top_n_words={word: ( tf_idf_transposed[j][0]) for j, word in enumerate(words)}
-    sort_words = sorted(top_n_words.items(), key=lambda x: x[1], reverse=True)[:n]
-    return sort_words
-
-
-
-def palabras_principales(df):
-    #función para aplicar en todo el dataset, trae sólo la primer palabra
-    docs_per_topic = df.groupby(['labels'], as_index = False).agg({'listado': ' '.join})
-    docs_per_topic.rename(columns={'listado': 'palabras_listado'}, inplace=True)
-    docs_per_topic['word']=0
-    for i in range(len(docs_per_topic)):
-        text=[docs_per_topic['palabras_listado'][i]]
-        tf_idf, count = tf(text)
-        docs_per_topic['word'][i]=extract_top_n_words_per_topic(tf_idf, count)[0][0]
-    df=df.merge(docs_per_topic,on='labels', how='left')
-    return df
+import plotly.graph_objs as go
+#grafico de barras
+def barra(df, y, title):
+    x= df.rating#['Administración', 'Gustavo Esquivel', 'Ismael Rimoldi','Juan Carlos Ferreira']
+    y = df[y]
+    fig_m_prog = go.Figure([go.Bar(x=x, y=y, text=y, textposition='auto',orientation='h', marker_color='rgb(26, 118, 255)')])
+    fig_m_prog.update_layout(paper_bgcolor="#ffffff", plot_bgcolor="#ffffff",
+                                 font={'color': "#111111", 'size': 14}, title=title)
+    fig_m_prog.update_yaxes(title='y', visible=False, showticklabels=False)
+    return fig_m_prog
 
 
 def grafico(df):
@@ -55,95 +35,112 @@ def grafico(df):
                 y="word",orientation='h')
     return fig
 
-
+#grafico de barras dimension
+def barra_dimension(df, y, title):
+    df=df.groupby(y).sum()
+    df['proporcion']=(df['count'] / df['count'].sum())*100
+    df.sort_values(by='proporcion', inplace=True)
+    x= df.proporcion#['Administración', 'Gustavo Esquivel', 'Ismael Rimoldi','Juan Carlos Ferreira']
+    y = df.index
+    fig_m_prog = go.Figure([go.Bar(x=x, y=y, text=np.round(x), textposition='auto',orientation='h', marker_color='rgb(26, 118, 255)',texttemplate='%{text}%')])
+    fig_m_prog.update_layout(paper_bgcolor="#ffffff", plot_bgcolor="#ffffff",
+                                 font={'color': "#111111", 'size': 14}, title=title)
+    fig_m_prog.update_yaxes( visible=True, showticklabels=True)
+    return fig_m_prog
 
 # Create connection object.
 # `anon=False` means not anonymous, i.e. it uses access keys to pull data.
 fs = s3fs.S3FileSystem(anon=False)
 
-file = "s3://datos-riverside/reporte.csv"
+file1='s3://datos-riverside/clasificacion_libros.xlsx'
 @st.cache
-def load_data(file1, file2, file3):
+def load_data(file1):
     with fs.open(file1, mode="rb") as f:
-        bert_clustering=pd.read_excel(f)
-    print(bert_clustering.info())
-    df=palabras_principales(bert_clustering)
-    with fs.open(file2, mode="rb") as f:
-        listados_all=pd.read_excel(f, index_col=0)
-    listados_all=listados_all.drop_duplicates(keep='first')
-    listados_all=listados_all.merge(df, left_on='list', right_on='listado')
-    clasificacion=listados_all.groupby(['isbn13', 'word', 'labels','palabras_listado']).agg({'count': 'sum' }).sort_values(by='count', ascending=False).reset_index()
-    with fs.open(file3, mode="rb") as f:
-        catalogo=pd.read_csv(f, usecols={'ean', 'sello', 'categoriapadre',  'categoria', 'titulo'},dtype={'ean':'str'})
-    listados_originales=listados_all.groupby(['isbn13'], as_index = False).agg({'list': ','.join })
-    listados_originales.rename(columns={'list': 'listados_originales'}, inplace=True)
-    clasificacion=clasificacion.merge(listados_originales, on='isbn13', how='left')
-    clasificacion=clasificacion.merge(catalogo, left_on='isbn13', right_on='ean')
-    clasificacion.drop('ean', axis=1, inplace=True)
-    return clasificacion
-
-st.title('Análisis de clustering por temas')
-
-file1='s3://datos-riverside/bert_clustering_20_5_con guion.xlsx'
-file2='s3://datos-riverside/listados_all.xlsx'
-file3='s3://datos-riverside/catalogo_actualizado.csv'
-with fs.open(file1, mode="rb") as f:
-    bert_clustering=pd.read_excel(f)
-print(bert_clustering.info())
-df=palabras_principales(bert_clustering)
-with fs.open(file2, mode="rb") as f:
-    listados_all=pd.read_excel(f, index_col=0)
-listados_all=listados_all.drop_duplicates(keep='first')
-listados_all=listados_all.merge(df, left_on='list', right_on='listado')
-clasificacion=listados_all.groupby(['isbn13', 'word', 'labels','palabras_listado']).agg({'count': 'sum' }).sort_values(by='count', ascending=False).reset_index()
-with fs.open(file3, mode="rb") as f:
-    catalogo=pd.read_csv(f, usecols={'ean', 'sello', 'categoriapadre',  'categoria', 'titulo'},dtype={'ean':'str'})
-listados_originales=listados_all.groupby(['isbn13'], as_index = False).agg({'list': ','.join })
-listados_originales.rename(columns={'list': 'listados_originales'}, inplace=True)
-clasificacion=clasificacion.merge(listados_originales, on='isbn13', how='left')
-clasificacion=clasificacion.merge(catalogo, left_on='isbn13', right_on='ean')
-clasificacion.drop('ean', axis=1, inplace=True)
+        df=pd.read_excel(f)
+        return df
 
 
-#clasificacion=load_data(,,'' )
-sellos = (clasificacion['sello'].unique())
-# usuario selecciona sello
-sello_seleccion = st.selectbox("Seleccionar sello", sellos)
-st.subheader(f"Libros del sello seleccionado:  {sello_seleccion}")
-seleccion=clasificacion.loc[clasificacion.sello==sello_seleccion]
+#-----------------------------------------------------------------------
+#APP
+st.set_page_config(layout="wide", page_title='Análisis clustering')
+st.markdown('')
+container = st.container()
+container.markdown('## 📙 Análisis de los temas de los libros, según Goodreads')
+
+#-------------------------------------------------------------------------------
+#DATOS
+
+clasificacion=load_data(file1)
 # usuario selecciona titulo
-titulos=(seleccion['titulo'].unique())
+titulos=(clasificacion['titulo_x'].unique())
 libro_seleccion = st.selectbox("Seleccionar libro", titulos)
-xlibro=clasificacion.loc[clasificacion.titulo==libro_seleccion]
+xlibro=clasificacion.loc[clasificacion.titulo_x==libro_seleccion]
 xlibro=xlibro.sort_values(by='count', ascending=False)
-xlibro=xlibro.loc[xlibro.labels != -1]
+trace1 = go.Bar(
+y=xlibro['rating'], # NOC stands for National Olympic Committee
+x=xlibro['publico_ojetivo'],
+name = 'Público_Ojetivo',text=np.round(xlibro['rating']),
+marker=dict(color='#FFD700') # set the marker color to gold
+)
+trace2 = go.Bar(
+y=xlibro['rating'],
+x=xlibro['popularidad'],
+name='popularidad',text=np.round(xlibro['rating']),
+marker=dict(color='#9EA0A1') # set the marker color to silver
+)
+trace3 = go.Bar(
+y=xlibro['rating'],
+x=xlibro['tema_literatura'],
+name='tema_literatura',text=np.round(xlibro['rating']),
+marker_color='crimson' # set the marker color to bronze
+)
+trace4 = go.Bar(
+y=xlibro['rating'], # NOC stands for National Olympic Committee
+x=xlibro['como_esta_escrito'],
+name = 'como_esta_escrito',text=np.round(xlibro['rating']),
+#marker=dict(color='#FFD700') # set the marker color to gold
+)
+trace5 = go.Bar(
+y=xlibro['rating'],
+x=xlibro['donde_esta_escrito'],
+name='donde_esta_escrito',text=np.round(xlibro['rating']),
+marker_color='rgb(26, 118, 255)' # set the marker color to silver
+)
+trace6 = go.Bar(
+y=xlibro['rating'],
+x=xlibro['cuando_esta_escrito'],
+name='cuando_esta_escrito',text=np.round(xlibro['rating']),
+marker=dict(color='#CD7F32') # set the marker color to bronze
+)
+data = [trace1, trace2, trace3 ,trace4, trace5, trace6]
+layout = go.Layout( height=800, width=1000,
+title='Participación de los temas sobre el total de votaciones',paper_bgcolor="#ffffff", plot_bgcolor="#ffffff",  yaxis=dict(
+        showgrid=False,
+        showline=False,
+        showticklabels=False,
+        domain=[0, 0.85]
+    ),xaxis_tickangle=-90)
+fig = go.Figure(data=data, layout=layout)
+fig=fig.update_traces(texttemplate='%{text}%')
+fig=fig.update_layout(uniformtext_minsize=6, uniformtext_mode='hide')
 
-#plot = grafico(xlibro)
-st.plotly_chart(grafico(xlibro))
-st.dataframe(xlibro)
+st.plotly_chart(fig)
+
+col1, col2= st.columns([4, 4])
+with col1:
+    st.plotly_chart(barra_dimension(xlibro, 'popularidad', 'Popularidad'))
+with col2:
+    st.plotly_chart(barra_dimension(xlibro, 'tema_literatura', 'Tema del Libro'))
+col1, col2= st.columns((2))
+with col1:
+    st.plotly_chart(barra_dimension(xlibro, 'cuando_esta_escrito', 'Cuándo está escrito'))
+with col2:
+    st.plotly_chart(barra_dimension(xlibro, 'donde_esta_escrito', 'Dónde está escrito'))
+col1, col2= st.columns((2))
+with col1:
+    st.plotly_chart(barra_dimension(xlibro, 'como_esta_escrito', 'Cómo está escrito'))
+with col2:
+    st.plotly_chart(barra_dimension(xlibro, 'publico_ojetivo', 'Público Objetivo'))
+
 #st.write(barh_variable(xlibro))
-
-# usuario selecciona label
-labels=(xlibro['labels'].unique())
-labels_seleccion = st.selectbox("Seleccionar similares por label", labels)
-st.subheader(f"Libros que pertenecen al mismo label:  {labels_seleccion}")
-label_=clasificacion.loc[clasificacion.labels==labels_seleccion]
-label_.sort_values(by=['count'], ascending=False)
-st.dataframe(label_)
-
-# usuario selecciona categoria
-categoria=(clasificacion['categoriapadre'].unique())
-categoria_seleccion = st.selectbox("Seleccionar similares por categoria", categoria)
-st.subheader(f"Libros que pertenecen a la misma categoria:  {categoria_seleccion}")
-categoria_=clasificacion.loc[clasificacion.categoriapadre==categoria_seleccion]
-categoria_=categoria_.astype({'labels': 'str'})
-categoria_=categoria_.fillna('-')
-categoria_isbn=categoria_.groupby(['isbn13', 'titulo'], as_index = False).agg({'labels': ','.join, 'categoria': ','.join, 'sello': ','.join})
-categoria_isbn.sort_values(by=['titulo'], ascending=False)
-st.dataframe(categoria_isbn)
-
-
-#isbn = st.text_area("ISBN para analizar", height=100)
-#xlibro=clasificacion.loc[clasificacion.isbn13==isbn]
-
-#clasificacion.loc[clasificacion.labels==55].sort_values(by='count', ascending=False)
+st.dataframe(xlibro)
